d10e39bb88b0e9165ca3d141ba06d7f9196af459
Sidong Charlie Feng <u6063820@anu.edu.au>
2020-07-04 15:07:11 +0800

    finialize mapping

--- server.js
+++ server.js
@@ -108,12 +108,12 @@ app.post('/save', function (req, res) {
     });
 });
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '0.0.0.0');
-console.log('Server running at http://127.0.0.1:1337/');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World\n');
+// }).listen(1337, '0.0.0.0');
+// console.log('Server running at http://127.0.0.1:1337/');
 
-// app.listen(8000, function(){
-//     console.log("Working on port 8000");
-// });
+app.listen(8000, function(){
+    console.log("Working on port 8000");
+});
